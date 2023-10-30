f3ed9530d558b5951f054a6c662426f2062a8194
unknown <johannjerman014@gmail.com>
2023-10-30 17:58:00 -0300

    a

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,25 +7,44 @@ import {
 } from "../../config/envs.js";
 
 export default {
-  production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-  },
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST_CONFIG,
+    dialect: "postgres",
+    migrationStorage: "sequelize",
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Puedes ajustar esto según tus requisitos de seguridad
+    //   },
+    // },
+    migrationStorageTableName: "sequelize_meta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "sequelize_data",
+    migrationStoragePath: "src/database/migrations",
+    seederStoragePath: "src/database/seeders",
   },
-  test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-  },
+  // production: {
+  // username: DB_USERNAME,
+  // password: DB_PASSWORD,
+  // database: DB_DATABASE,
+  // host: DB_HOST,
+  // dialect: DB_DIALECT,
+  // },
+  // development: {
+  // username: DB_USERNAME,
+  // password: DB_PASSWORD,
+  // database: DB_DATABASE,
+  // host: DB_HOST,
+  // dialect: DB_DIALECT,
+  // },
+  // test: {
+  // username: DB_USERNAME,
+  // password: DB_PASSWORD,
+  // database: DB_DATABASE,
+  // host: DB_HOST,
+  // dialect: DB_DIALECT,
+  // },
 };
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -32,7 +32,15 @@ import {
 //     port: 5432,
 //   });
 // }
-export const sequelize = new Sequelize(DB_CONNECTIONSTRING);
+export const sequelize = new Sequelize(DB_CONNECTIONSTRING, {
+  //dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Ajusta esto según tus requisitos de seguridad
+    },
+  },
+});
 // export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
 //   host: "localhost",
 //   dialect:
